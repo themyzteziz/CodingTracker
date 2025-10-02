e708cdace556df7124c48ac2a5ad2c15f6b87a10
themyzteziz <markus.siteus@hotmail.com>
2025-10-02 14:34:19 +0200

    Updated Program.cs

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Markus");
-Console.WriteLine("Tjenixen Lars");
+﻿using System;
+
+namespace CodingTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Markus");
+        }
+    }
+}
